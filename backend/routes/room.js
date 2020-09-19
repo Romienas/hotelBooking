0b148a70f5b8c6e63a7d580b317784b5c7363850
Roman Juchimčuk <romienas@Romans-MacBook-Pro.local>
2020-09-19 21:57:35 +0300

    create rooute to addRoom

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -1,1 +1,9 @@
 const express = require('express')
+const router = express.Router()
+
+//Add hotel room to database
+router.post('/addRoom', (req, res, next) => {
+    res.send(req.body)
+})
+
+module.exports = router
