4ca29b864ea8b31450e17d64458fb9699da7d83a
Roman Juchimčuk <romienas@Romans-MacBook-Pro.local>
2020-09-21 08:12:02 +0300

    add fonts, add title to admin page

--- frontend/src/routes/admin.js
+++ frontend/src/routes/admin.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Admin() {
     return (
-        <div>
-            login
+        <div className='admin'>
+            <div className='admin__login'>
+                <div className='admin__login-title'>
+                    Sign in to manage your hotel
+                </div>
+            </div>
         </div>
     )
 }
