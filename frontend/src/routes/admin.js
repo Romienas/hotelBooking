965b31a1205c57e06527503a6e0cb0cb740ecf3a
Roman Juchimčuk <romienas@Romans-MacBook-Pro.local>
2020-09-23 08:12:59 +0300

    add inout to admin page

--- frontend/src/routes/admin.js
+++ frontend/src/routes/admin.js
@@ -10,6 +10,11 @@ function Admin() {
                 <div className='admin__login-title'>
                     Sign in to manage your hotel
                 </div>
+                <div>
+                    <p>This is demo version. To sign in use:</p>
+                    <p>email: admin@mail.com</p>
+                    <p>password: amdin1admin</p>
+                </div>
                 <div className='admin__login-username'>
                     <div className='admin__login-input-title'>
                         Username
